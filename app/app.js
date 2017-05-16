8241c7727c909109283e09165bf0e07ad2955b21
Injulkar, Nilesh (DS) <Nilesh.Injulkar@cdk.com>
2017-05-16 20:07:05 +0530

    Add book to storage and show list of stored books

--- app/app.js
+++ app/app.js
@@ -55,18 +55,45 @@ angular.module('chrome.plugin.trynew', ['ngMaterial', 'ngMdIcons'])
       });    
     };
 }])
-.controller('TryNewBookController', ['$q', '$timeout', 'BookAPIService', 
-                                     function($q, $timeout, BookAPIService) {
+.service('StorageService', [ function() {
+  var book_key = 'TryNewBooks';
+  
+  this.getBooks = function(callBack) {
+    chrome.storage.sync.get(book_key, function(data) {
+      console.log('LOADED', data);
+      callBack(data[book_key]);
+    });
+  };
+  
+  this.setBooks = function(books, callBack) {
+    var bookList = {};
+    bookList[book_key] = books;
+    chrome.storage.sync.set(bookList, function() {
+      callBack();
+    });
+  }
+  
+}])
+.controller('TryNewBookController', ['$q', '$timeout', 'BookAPIService', 'StorageService', '$animate',
+                                     function($q, $timeout, BookAPIService, StorageService, $animate) {
     var vm = this;
+                                             $animate.enabled(true);
     vm.selectedBook;                                   
     vm.books =  [ 
       { 'title' : 'Cosmos', 'author' : 'Carl Sagan'},
       { 'title' :  'The brothers karamzov', 'author' : 'Dastovesky'},
       { 'title' :  'Sapiens', 'author' : 'Harari'}
     ];
-
+                                       
     vm.newSelectedBook;
     vm.searchText = '';
+    vm.myBooks = [];
+            
+    StorageService.getBooks(function(books) {
+      console.log(books);
+      vm.myBooks = (books && books.length) ? books : [];
+      console.log(vm.myBooks);
+    });
 
     vm.querySearch = function (query) {
       var deferred = $q.defer();
@@ -82,14 +109,26 @@ angular.module('chrome.plugin.trynew', ['ngMaterial', 'ngMdIcons'])
       vm.selectedBook = book;
     };
                                        
+    vm.addNewBook = function() {
+      console.log('BEFORE ADD', vm.myBooks);
+      vm.myBooks.unshift(vm.newSelectedBook);
+      StorageService.setBooks(vm.myBooks, function() {
+        console.log('ADDED');
+      });
+    };
+                                       
 }])
 .config(function($mdIconProvider) {
   $mdIconProvider
     .icon('magnify', 'images/icons/magnify.svg', 24)
     .icon('dots-vertical', 'images/icons/dots-vertical.svg', 24)
+    .icon('dots-horizontal', 'images/icons/dots-horizontal.svg', 24)
     .icon('book', 'images/icons/book-open-page-variant.svg', 24)
     .icon('movie-tv', 'images/icons/message-video.svg', 24)
     .icon('music', 'images/icons/music.svg', 24)
     .icon('star', 'images/icons/star.svg', 24)
+    .icon('goodreads', 'images/icons/goodreads.svg', 12)
+    .icon('delete', 'images/icons/delete.svg', 12)
+    .icon('open', 'images/icons/open-in-new.svg', 12)
     .icon('add', 'images/icons/plus.svg', 24);
 });
